490640778d1e0ea260d9f0c8272a422167b0db2f
Sahil-4 <www.sahil.875020@gmail.com>
2022-02-10 00:07:12 +0530

    Improved UI

--- src/Components/Game.js
+++ src/Components/Game.js
@@ -1,7 +1,10 @@
 import React from "react";
-import rockImage from "./assets/rock.png";
-import paperImage from "./assets/paper.png";
-import scissorImage from "./assets/scissor.png";
+const rockImage =
+  "https://firebasestorage.googleapis.com/v0/b/storage4otherprojs.appspot.com/o/RPSG%2Fv2%2Frock.png?alt=media&token=e7590619-3886-48da-bcf3-3ae8cfb22a15";
+const paperImage =
+  "https://firebasestorage.googleapis.com/v0/b/storage4otherprojs.appspot.com/o/RPSG%2Fv2%2Fpaper.png?alt=media&token=83ba8215-215c-461e-92c8-dc423d4569b9";
+const scissorImage =
+  "https://firebasestorage.googleapis.com/v0/b/storage4otherprojs.appspot.com/o/RPSG%2Fv2%2Fscissor.png?alt=media&token=6492198f-449a-4836-b868-799718209f08";
 
 const Game = () => {
   return (
@@ -9,13 +12,11 @@ const Game = () => {
       <h1 className="game-title">Rock Paper Scissor</h1>
 
       <div className="score-board W100">
-        {/* <div className="choice"> */}
         <div className="your-score-board score-boards">
           <h2>You choose : Rock</h2>
           <h2>Your Points : 8</h2>
         </div>
 
-        {/* <div className="points"> */}
         <div className="opponent-score-board score-boards">
           <h2>Opponent choose : Scissor</h2>
           <h2>Opponent's Points : 5</h2>
@@ -58,12 +59,19 @@ const Game = () => {
 
       <div className="chat-box W100">
         <form method="POST">
-          <input
+          <div
             type="text"
-            disabled
             id="received-chat-message"
             className="message-box W100"
-          />
+          >
+            <p>You : Hello</p>
+            <p>Alpha : Hey</p>
+            <p>You : Hello</p>
+            <p>Alpha : Hey</p>
+            <p>You : Hello</p>
+            <p>Alpha : Hey</p>
+            <p>You : Hello</p>
+          </div>
           <input type="text" id="send-chat-message" className="message-box" />
           <button id="emoji-button" className="chat-button">
             emoji
