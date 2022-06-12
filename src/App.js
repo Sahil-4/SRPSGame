a7a8e35d4b49e79384adca8a0b427f0e8b185111
Sahil-4 <www.sahil.875020@gmail.com>
2022-06-12 21:05:52 +0530

    removed test page

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@ import Home from "./Pages/Home";
 import Help from "./Pages/Help";
 import About from "./Pages/About";
 import Login from "./Pages/Login";
-import Test from "./Pages/Test";
 import GameMultiplayer from "./Pages/GameMultiplayer";
 import GameSingleplayer from "./Pages/GameSingleplayer";
 import PageNotFound from "./Pages/PageNotFound";
@@ -23,7 +22,6 @@ function App() {
           <Route exact path="/play" element={<GameSingleplayer />} />
           <Route exact path="/play-two" element={<GameMultiplayer />} />
           <Route exact path="/help" element={<Help />} />
-          <Route exact path="/test" element={<Test />} />
           <Route exact path="/about" element={<About />} />
           <Route exact path="*" element={<PageNotFound />} />
         </Routes>
